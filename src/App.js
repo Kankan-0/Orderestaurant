a3b3e5fdd06bdddf055ff1ba0fdf45c8205418b3
Kankan Barman <kankandev@protonmail.com>
2021-09-19 11:10:29 +0530

    Initial setup with CRA and tailwind

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Layout/Header";
+
+function App() {
+  return (
+    <div>
+      <h1 className="bg-red-500">Hello World!</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
